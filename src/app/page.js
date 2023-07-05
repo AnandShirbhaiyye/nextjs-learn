1b47108ffdbb6067fd38ceeaedf7def887e739f8
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-05 10:36:33 +0530

    make component and reused component in next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Hello Anand...</h1>
+      <User/>
+      <User/>
+      <User/>
+      <User/>
+      <User/>
     </main>
-  )
+  );
 }
+
+const User = () => {
+  return (
+    <div>
+      <h2>My Name is Ujwal...</h2>
+    </div>
+  )
+};
