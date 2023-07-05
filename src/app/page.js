2a5ee75b2454a089378866e252ad95491cd2b09f
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-05 11:06:23 +0530

    implement events and functions in next js

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,19 @@
+'use client'
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const apple = (item)=>{
+    alert(item)
+  }
   return (
     <main className={styles.main}>
       <h1>Hello Anand...</h1>
-      <User name="Ujwal" age={23}/>
       <User name="Anand" age={23}/>
-      <User name="Ankit" age={24}/>
       <User name="Kajal" age={26}/>
       <User name="Kanchan" age={27}/>
+      <button onClick={()=>alert("Hello Next Js")}>Click me..</button>
+      <button onClick={()=>apple("hey...nextjs..how are you")}>Click me2</button>
     </main>
   );
 }
