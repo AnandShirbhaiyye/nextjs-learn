f67af8c360051c93f2785df022bcd8dedeb9e59d
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-11 17:20:52 +0530

    create a foler for the new page

--- src/app/page.js
+++ src/app/page.js
@@ -4,27 +4,35 @@ import styles from "./page.module.css";
 
 export default function Home() {
 
-  const [item, setItem] = useState("KajalKanchan")
+  // const [item, setItem] = useState("KajalKanchan")
 
   // const apple = (item)=>{
   //   alert(item)
   // }
 
-  const state=()=>{
-    setItem("AnandUjwal")
-  }
+  // const state=()=>{
+  //   setItem("AnandUjwal")
+  // }
+
+  //   const InnerComp=()=>{
+  //     return(
+  //       <h1>innerCompoent</h1>
+  //     )
+  //   }
 
   return (
     <main className={styles.main}>
+      <h1>Basic Routing | Make new Page</h1>
       {/* <h1>Hello Anand...</h1>
       <User name="Anand" age={23}/>
       <User name="Kajal" age={26}/>
       <User name="Kanchan" age={27}/>
       <button onClick={()=>alert("Hello Next Js")}>Click me..</button>
       <button onClick={()=>apple("hey...nextjs..how are you")}>Click me2</button> */}
-      <h1>Hello {item}</h1>
-      <button onClick={()=>state()}>State</button>
-    </main>
+    {/* //   <h1>Hello {item}</h1>
+    //   <button onClick={()=>state()}>State</button>
+    //   <InnerComp/> */}
+     </main>
   );
 }
 
