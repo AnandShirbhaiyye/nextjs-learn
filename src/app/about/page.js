424791f95789ff8ff183bfea6d9f1ccef626d0d9
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-19 18:54:48 +0530

    added navigation in about page.js and page js

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,15 @@
+"use client";
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from "next/navigation";
 
 export default function About () {
+  const router = useRouter();
   return (
     <div> 
     <h2>About page</h2>
-    <Link href="/">HomePage</Link>
+    {/* <Link href="/">HomePage</Link> */}
+    <button onClick={()=>router.push("/")}>go to home page</button>
   </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
-'use client'
+"use client";
 import { useState } from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
-import { useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-
-  const router = useRouter(); 
+  const router = useRouter();
+  const  navigate  = (name)=>{
+    router.push(name)
+  }
 
   // const [item, setItem] = useState("KajalKanchan")
 
@@ -27,10 +29,18 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Basic Routing | Make new Page</h1>
-      <Link href="login">Go to login page</Link><br/><br/>
-      <Link href="about">Go to about page</Link><br/><br/>
+      <Link href="login">Go to login page</Link>
+      <br />
+      <br />
+      <Link href="about">Go to about page</Link>
+      <br />
+      <br />
 
-      <button onClick={()=>router.push("/login")}>Login Page</button>
+      {/* <button onClick={()=>router.push("/login")}>Login Page</button>
+      <button onClick={()=>router.push("/about")}>About Page</button> */}
+      <button onClick={()=>navigate('/login')}>Login Page</button>
+      <button onClick={()=>navigate('/about')}>About Page</button>
+
       {/* <button>Login Page</button> */}
       {/* <h1>Hello Anand...</h1>
       <User name="Anand" age={23}/>
@@ -38,10 +48,10 @@ export default function Home() {
       <User name="Kanchan" age={27}/>
       <button onClick={()=>alert("Hello Next Js")}>Click me..</button>
       <button onClick={()=>apple("hey...nextjs..how are you")}>Click me2</button> */}
-    {/* //   <h1>Hello {item}</h1>
+      {/* //   <h1>Hello {item}</h1>
     //   <button onClick={()=>state()}>State</button>
     //   <InnerComp/> */}
-     </main>
+    </main>
   );
 }
 
