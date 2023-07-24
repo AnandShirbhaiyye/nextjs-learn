e414c127e10c4d270ed41702b270eb069fc32c6c
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-24 16:13:41 +0530

    added navigate and nested routing in login folder

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,11 +1,24 @@
+"use client"
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
 import React from 'react';
 
 export default function Login() {
+  const router = useRouter();
+
+  const navigate = (page)=>{
+    router.push("/login/" +page)
+  }
   return (
     <div> 
       <h2>Login page</h2>
-      <Link href="/">HomePage</Link>
+      <Link href="/">HomePage</Link><br/><br/>
+
+      {/* <button onClick={()=>router.push("/login/loginstudent")}>Go to Student Login Page</button> */}
+      <button onClick={()=>navigate("loginstudent")}>Go to Student Login Page</button>
+      <br/><br/>
+      <button onClick={()=>navigate("loginteacher")}>Go to Teacher Login Page</button>
     </div>
     
   )
