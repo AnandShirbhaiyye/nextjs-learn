c12ddeeec46094889d9c3297f45877538d99f827
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-26 18:54:22 +0530

    added link in layout component

---
+++ src/app/login/layout.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function layout({children }){
+    return(
+        <div>
+            <ul>
+                <li>
+                    <Link href="/login">Login Main</Link>
+                </li>
+
+                <li>
+                    <Link href="/login/loginstudent"> Student Login </Link>
+                </li>
+
+                <li>
+                    <Link href="/login/loginteacher"> Teacher Login </Link>
+                </li>
+            </ul>
+            {children}
+        </div>
+    )
+}
