ea562cd994d70977a848100763e745c601eb8897
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-28 23:15:45 +0530

    create studentList folder

---
+++ src/app/studentList/page.js
@@ -0,0 +1,13 @@
+export default function StudentList() {
+  return (
+  <div>
+    <h1>Student List</h1>
+    <ul>
+        <li>Anand</li>
+        <li>Kajal</li>
+        <li>Kanchan</li>
+        <li>Ujwal</li>
+    </ul>
+  </div>
+  )
+}
