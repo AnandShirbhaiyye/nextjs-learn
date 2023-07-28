7653cdeed461c85547230c4454253a01a279337a
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-28 23:27:46 +0530

    create student folder

--- src/app/studentList/[student]/page.js
+++ src/app/studentList/[student]/page.js
@@ -1,23 +1,7 @@
-import Link from "next/link";
-
 export default function StudentList() {
   return (
     <div>
       <h1>Student List</h1>
-      <ul>
-        <li>
-          <Link href="/studentList/1">Anand</Link>
-        </li>
-        <li>
-          <Link href="/studentList/2">Kajal</Link>
-        </li>
-        <li>
-          <Link href="/studentList/3">Kanchan</Link>
-        </li>
-        <li>
-          <Link href="/studentList/4">Ujwal</Link>
-        </li>
-      </ul>
     </div>
   );
 }
