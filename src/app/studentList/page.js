d0e037982573f07f041e6c436b80ac244626dac8
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-28 23:21:19 +0530

    added Link tag in studentlist

--- src/app/studentList/page.js
+++ src/app/studentList/page.js
@@ -1,13 +1,23 @@
+import Link from "next/link";
+
 export default function StudentList() {
   return (
-  <div>
-    <h1>Student List</h1>
-    <ul>
-        <li>Anand</li>
-        <li>Kajal</li>
-        <li>Kanchan</li>
-        <li>Ujwal</li>
-    </ul>
-  </div>
-  )
+    <div>
+      <h1>Student List</h1>
+      <ul>
+        <li>
+          <Link href="/1">Anand</Link>
+        </li>
+        <li>
+          <Link href="/2">Kajal</Link>
+        </li>
+        <li>
+          <Link href="/3">Kanchan</Link>
+        </li>
+        <li>
+          <Link href="/4">Ujwal</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
