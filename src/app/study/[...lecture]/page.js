a565b5481d176b81ff407228359abf4a5708c94c
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-07-31 23:36:33 +0530

    added segment route in next js

--- src/app/study/[...lecture]/page.js
+++ src/app/study/[...lecture]/page.js
@@ -1,7 +1,11 @@
-export default function Lecture(){
+"use client";
+export default function Lecture({params}){
+    console.log(params);
     return(
        <div>
-        <h1> Lectures 1</h1>
+        <h1> {params.lecture[0]}</h1>
+        <h1> {params.lecture[1]}</h1>
+        <h1> {params.lecture[2]}</h1>
        </div>
     )
 }
