1190909f6b4d0cd617b20a0e93519beba22f5dc4
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-08-08 22:54:07 +0530

    added middleware in nextjs

--- src/middleware.js
+++ src/middleware.js
@@ -2,5 +2,13 @@ import {NextResponse} from "next/server";
 
 export function middleware(request){
 
-   return NextResponse.redirect(new URL("/login", request.url))
+
+    // if(request.nextUrl.pathname!="/login"){
+      return NextResponse.redirect(new URL("/login", request.url))
+    // }
+   
+}
+
+export const config={
+    matcher:["/about/:path*", "/studentList/:path*" ]
 }
